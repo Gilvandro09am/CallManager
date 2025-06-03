be0edae516462a224d5788bb176233b2fcc76270
JoseIvanildo <joseivanildomarques16@gmail.com>
2025-06-03 16:20:30 -0300

    Sistema-De-Rotas

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from "./components/Login";
 import AvaliacaoChamado from "./components/AvaliacaoChamado";
-//import RespostaChamado from './components/RespostaChamado';
-//import AlterarSenha from "./components/AlterarSenha";
-//import ChamadosTecnico from './components/ChamadosTecnico';
-//import FormularioChamado from './components/FormularioChamado';
+import RespostaChamado from './components/RespostaChamado';
+import AlterarSenha from "./components/AlterarSenha";
+import ChamadosTecnico from './components/ChamadosTecnico';
+import FormularioChamado from './components/FormularioChamado';
 
 
-function App() {
+ function App() {
   return (
-    <div>
-      <AvaliacaoChamado />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/avaliar-chamado" element={<AvaliacaoChamado />} />
+        <Route path="/chamados" element={<ChamadosTecnico />} />
+        <Route path="/resposta-chamado" element={<RespostaChamado />} />
+        <Route path="/formulario-chamado" element={<FormularioChamado />} />
+        <Route path="/alterar-senha" element={<AlterarSenha />} />
+        </Routes>
+    </BrowserRouter>
   );
-}
+} 
 
 export default App;
